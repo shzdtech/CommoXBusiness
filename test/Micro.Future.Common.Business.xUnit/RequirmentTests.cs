a44266a09964831332311a1034b490c6f120fd3a
aliceminli <aliceminli@hotmail.com>
2016-08-02 22:11:29 +0800

    Add User Register Unit Case

--- test/Micro.Future.Common.Business.xUnit/RequirmentTests.cs
+++ test/Micro.Future.Common.Business.xUnit/RequirmentTests.cs
@@ -1,9 +1,12 @@
 ﻿using Micro.Future.Commo.Business.Abstraction.BizInterface;
 using Micro.Future.Commo.Business.Requirement.Handler;
+using Micro.Future.Commo.Business.Abstraction.BizObject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xunit;
+using Micro.Future.Business.Common;
 
 namespace Micro.Future.Common.Business.xUnit
 {
@@ -14,5 +17,21 @@ namespace Micro.Future.Common.Business.xUnit
             IRequirementManager manager = new RequirementManager();
             manager.AddRequirementInfo(new Commo.Business.Abstraction.BizObject.RequirementInfo());
         }
+
+        [Fact]
+        public void RegisterUsersTest()
+        {
+            UserInfo userA = new UserInfo();
+            userA.Name = "testuserA";
+            userA.Password = "111111";
+            userA.Phone = "66666666";
+
+            UserManager userManagerA = new UserManager();
+            BizTResult<bool> isRegistered = userManagerA.Register(userA);
+            Assert.True(isRegistered.Result);
+                    
+        }
     }
+
+   
 }
